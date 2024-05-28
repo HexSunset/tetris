845201e5da24ac59fd83e81780f3b99efc4ae92f
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2024-05-28 23:30:03 +0300

    Functions for drawing the game

---
+++ src/draw.c
@@ -0,0 +1,58 @@
+#include "draw.h"
+#include "tetris.h"
+
+Color block_color(BlockType bt) {
+	switch (bt) {
+	case BLOCK_NONE:
+		return BACKGROUND_COLOR;
+	case BLOCK_I:
+		return SKYBLUE;
+	case BLOCK_O:
+		return YELLOW;
+	case BLOCK_T:
+		return PURPLE;
+	case BLOCK_S:
+		return GREEN;
+	case BLOCK_Z:
+		return RED;
+	case BLOCK_J:
+		return DARKBLUE;
+	case BLOCK_L:
+		return ORANGE;
+	}
+
+	return BLACK;
+}
+
+void draw_block(int x, int y, Color color) {
+	int top_left_x_coord = x * GRID_PIXELS + GRID_BORDER_PIXELS;
+	int top_left_y_coord = (GRID_HEIGHT - y - 1) * GRID_PIXELS + GRID_BORDER_PIXELS;
+	int block_width = GRID_PIXELS - GRID_BORDER_PIXELS;
+	int block_height = GRID_PIXELS - GRID_BORDER_PIXELS;
+
+	DrawRectangle(top_left_x_coord, top_left_y_coord, block_width, block_height, color);
+}
+
+void draw_grid(Grid *grid) {
+	for (int x = 0; x < GRID_WIDTH; x++) {
+		for (int y = GRID_HEIGHT - 1; y >= 0; y--) {
+			BlockType bt = grid->blocks[x][y];
+			draw_block(x, y, block_color(bt));
+		}
+	}
+}
+
+void draw_piece(Piece piece, int x, int y) {
+	for (int x_i = 0; x_i < PIECE_WIDTH; x_i++) {
+		for (int y_i = 0; y_i < PIECE_HEIGHT; y_i++) {
+			int grid_x = x + x_i;
+			int grid_y = y + y_i;
+
+			BlockType block = piece.shape[PIECE_HEIGHT - y_i - 1][x_i];
+
+			if (block == BLOCK_NONE) continue;
+
+			draw_block(grid_x, grid_y, block_color(block));
+		}
+	}
+}
---
+++ src/draw.h
@@ -0,0 +1,18 @@
+#ifndef TETRIS_DRAW_H
+#define TETRIS_DRAW_H
+
+#include <raylib.h>
+
+#include "tetris.h"
+
+#define GRID_PIXELS 25
+#define GRID_BORDER_PIXELS 3
+
+#define BACKGROUND_COLOR (Color) {0x16, 0x16, 0x16, 0xFF}
+
+Color block_color(BlockType bt);
+void  draw_block(int x, int y, Color color);
+void  draw_grid(Grid *grid);
+void draw_piece(Piece piece, int x, int y);
+
+#endif // TETRIS_DRAW_H
