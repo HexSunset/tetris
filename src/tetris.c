a3165edab23f589c28700507900e02647a816218
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2024-05-31 21:45:50 +0300

    tetrominos.h, rotation of 0 always

--- src/tetris.c
+++ src/tetris.c
@@ -111,11 +111,11 @@ const Shape* get_shape(Piece piece) {
 }
 
 Piece get_random_piece() {
-	BlockType piece_type = rand() % DIFFERENT_PIECES;
-	BlockType rotation = rand() % NUM_ROTATIONS;
+	BlockType piece_type = rand() % BLOCK_TYPE_COUNT;
+	BlockType rotation = 0;
 
 	while (piece_type == BLOCK_NONE)
-		piece_type = rand() % DIFFERENT_PIECES;
+		piece_type = rand() % BLOCK_TYPE_COUNT;
 
 	return get_piece(piece_type, rotation);
 }
