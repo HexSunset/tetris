a58af33a08759093c0d6fbc2a3098783a22a4fe2
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2024-05-31 21:46:58 +0300

    Remove comments

--- src/main.c
+++ src/main.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include <raylib.h>
 #include <math.h>
-#include <time.h>
 #include <stdlib.h>
-
+#include <time.h>
 #include "draw.h"
 #include "tetris.h"
-
-//Grid gs.grid = {0};
 
 int main() {
 	GameState gs = init_gamestate();
@@ -17,20 +14,6 @@ int main() {
 	srand(time(0));
 
 	SetTargetFPS(0);
-
-	//float piece_x = PIECE_STARTING_X;
-	//float piece_y = PIECE_STARTING_Y;
-
-	//Piece current_piece = get_random_piece();
-
-	//float level_drop_speed = 4.0;
-	//float fast_drop_speed = 12.0;
-
-	//float current_drop_speed = level_drop_speed;
-
-	//bool paused = false;
-	//bool game_over = false;
-	//bool show_fps = false;
 
 	Image game_over_overlay_img = GenImageColor(GRID_WIDTH * GRID_PIXELS, GRID_HEIGHT * GRID_PIXELS, (Color) {0, 0, 0, 0x54});
 	Texture2D game_over_overlay_tex = LoadTextureFromImage(game_over_overlay_img);
