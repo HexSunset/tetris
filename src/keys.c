6b414c3e04807b8d5d80151a390565e52fcfaa2e
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2024-07-29 19:43:24 +0300

    Change some default binds

--- src/keys.c
+++ src/keys.c
@@ -7,11 +7,11 @@ void initialize_default_keys(Keymap *keys) {
 	(*keys)[ACTION_ROTATE_CLOCKWISE] = KEY_X;
 	(*keys)[ACTION_ROTATE_COUNTERCLOCKWISE] = KEY_Z;
 	(*keys)[ACTION_SOFT_DROP] = KEY_DOWN;
-	(*keys)[ACTION_PAUSE] = KEY_ESCAPE;
+	(*keys)[ACTION_PAUSE] = KEY_SPACE;
 	(*keys)[ACTION_RESTART] = KEY_R;
 	(*keys)[ACTION_QUIT] = KEY_Q;
 	(*keys)[ACTION_MENU_UP] = KEY_UP;
 	(*keys)[ACTION_MENU_DOWN] = KEY_DOWN;
-	(*keys)[ACTION_MENU_SELECT] = KEY_ENTER;
+	(*keys)[ACTION_MENU_SELECT] = KEY_X;
 	(*keys)[ACTION_TOGGLE_FPS] = KEY_F;
 }
