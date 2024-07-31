600f5089934175fcea1a744760337d2ba6bbbabe
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2024-08-01 00:01:33 +0300

    Changed up the build script, added run option

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -xe
-
 SRC="src"
 
 BUILD="build"
@@ -12,4 +10,12 @@ CCFLAGS="-Wall -Wextra -g"
 
 INCLUDE="-lraylib -lm"
 
+echo "+ Building '$BUILD/$NAME'..."
 gcc $CCFLAGS $SRC/*.c -o $BUILD/$NAME $INCLUDE
+echo "+ Built '$BUILD/$NAME'!"
+
+if [[ $1 == "run" ]]; then
+    echo "+ Running '$BUILD/$NAME'"
+    $BUILD/$NAME
+    echo "+ Finished running '$BUILD/$NAME'"
+fi
